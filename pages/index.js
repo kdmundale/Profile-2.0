2de00ba33c84949b976b200da1514f71327cad21
Katherine D Mundale <55110504+kdmundale@users.noreply.github.com>
2023-07-25 11:24:32 -0400

    Update index.js
    Updating landing page

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home(){
 
   let blah="bringing unique perspective to programming and development"
 
-  let content = "I have spent the last two decades building relationships.\nCultivating positive spaces for discussions and elevating experiences is my passion.\nI know people.\nNow, I want to build things that make their lives better.\nI offer a unique perspective\n \n-I'm not your typical programmer-\n \nand that makes me an asset."
+  let content = "I have spent the last two decades building relationships.\nCultivating positive spaces for discussions and elevating experiences is my passion.\nI know people.\nNow, I want to build systems that make their lives better.\n \nI offer a unique perspective\n \nand that makes me an asset."
 
   function NewlineText(props) {
   const text = props.text;
